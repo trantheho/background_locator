0d254743f082a41d1929830b98b0ce4accd38a32
trantheho <47885306+trantheho@users.noreply.github.com>
2022-08-30 16:58:45 +0700

    Update LocationParserUtil.kt

--- android/src/main/kotlin/rekab/app/background_locator/provider/LocationParserUtil.kt
+++ android/src/main/kotlin/rekab/app/background_locator/provider/LocationParserUtil.kt
@@ -3,6 +3,7 @@ package rekab.app.background_locator.provider
 import android.location.Location
 import android.os.Build
 import com.google.android.gms.location.LocationResult
+import android.os.PowerManager
 import rekab.app.background_locator.Keys
 import java.util.HashMap
 
